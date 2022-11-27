474679c95ae3fb8925b9bd086a531381601de624
million-art <millionmulugeta09@gmail.com' git config --global user.name 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 git config --global user.email "millionmulugeta09@gmail.com>
2022-11-27 14:45:05 +0300

    styles added toproduct list

--- ecomerce/src/pages/Product.jsx
+++ ecomerce/src/pages/Product.jsx
@@ -16,18 +16,37 @@ export const Products = () => {
     flex-direction:row;
     width:100%;
     height:350px;
-    background-color:gray;
+    background-color:white;
+    `
+    const Title=styled.div`
+     display:flex;
+    flex-direction:column;
+    
+      
     `
     const ImageContainer=styled.div`
     flex:1;
     display:flex;
     align-items:center;
     justify-content: center;
+
+    img{
+      width:100%;
+      height:90%;
+      padding:20px 50px;
+    }
+
     `
-    const Title=styled.div`
+    
+    const Discription=styled.div`
     flex:1;
     display:flex;
-    align-items:center;
+    flex-direction:column;
+    justify-content: center;
+     gap:10px;
+    h1{
+      font-weight:200;
+    }
    
     ` 
   return (
@@ -35,12 +54,18 @@ export const Products = () => {
         <Announcement />
         <NavBar />
         <Wrapper>
+          
             <ImageContainer>
-                <h1>hellow world</h1>
+            
+                 <img src='https://cdn1.smartprix.com/rx-iD8y25iVP-w1200-h1200/D8y25iVP.jpg' />
              </ImageContainer>
-             <Title>
+             <Discription>
+                <Title>
+                <h1>IPHONE 14</h1>
+                </Title>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium soluta sint aperiam blanditiis iste asperiores possimus nostrum culpa magnam perferendis</p>
-             </Title>
+                <h1>$10</h1>
+             </Discription>
         </Wrapper>
         <NewsLetter />
         <Footer />
